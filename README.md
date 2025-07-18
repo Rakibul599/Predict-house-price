bd754b6b130f60d7a145852c46120b72e9036289
Rakibul Alam <rakibulalam59900@gmail.com>
2025-07-18 19:59:11 +0600

    env added

--- README.md
+++ README.md
@@ -1,7 +1,10 @@
 # Predict-house-price
 
-mkdir myproject
-cd myproject
+
+cd back-end
 python3.11 -m venv .venv
 
 source .venv/bin/activate
+
+pip install -r requirements.txt
+
--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -61,7 +61,7 @@ function App() {
   async function handlePredictprice() {
     try {
       setLoading(true)
-      const response = await axios.post("http://127.0.0.1:5000/predict_price", formData, {
+      const response = await axios.post(`${import.meta.env.VITE_API}/predict_price`, formData, {
         headers: {
           "Content-Type": "application/json"
         }
